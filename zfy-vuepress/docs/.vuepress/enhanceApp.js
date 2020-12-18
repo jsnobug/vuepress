46d450cc964189e8d8adf56d965eb198b6780287
zhengfy <zhengfy@zjtoman.com>
2020-12-18 17:22:46 +0800

    重构vuepressxx

--- zfy-vuepress/docs/.vuepress/enhanceApp.js
+++ zfy-vuepress/docs/.vuepress/enhanceApp.js
@@ -1,8 +1,13 @@
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+const context = require.context('./components', false, /\.vue$/)
 
 export default ({
   Vue
 }) => {
   Vue.use(ElementUI)
+  context.keys().forEach(key => {
+    let component = context(key).default
+    Vue.component(component.name, component)
+  })
 }
